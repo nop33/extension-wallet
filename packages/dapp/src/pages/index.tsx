28de39a292ebb1fe0b16903a5905a461294205cf
Ilias Trichopoulos <hlias.nop@gmail.com>
2024-03-13 09:52:18 +0800

    Connect to WC manually (not web3-react)

--- packages/dapp/src/pages/index.tsx
+++ packages/dapp/src/pages/index.tsx
@@ -1,7 +1,8 @@
+import { WalletConnectProvider } from "@alephium/walletconnect-provider"
 import { useWallet } from "@alephium/web3-react"
 import type { NextPage } from "next"
 import Head from "next/head"
-import { useEffect, useState } from "react"
+import { useState } from "react"
 
 import { TokenDapp } from "../components/TokenDapp"
 import styles from "../styles/Home.module.css"
@@ -12,12 +13,40 @@ const Home: NextPage = () => {
   const [network, setNetwork] = useState<string | undefined>(undefined)
   const [isConnected, setConnected] = useState(false)
 
+  const connect = async () => {
+    const wcProvider = await WalletConnectProvider.init({
+      projectId: "6e2562e43678dd68a9070a62b6d52207",
+      networkId: "testnet",
+      onDisconnected: () => Promise.resolve(),
+    })
+
+    wcProvider.on("displayUri", (uri) => console.log(uri))
+
+    await wcProvider.connect()
+
+    if (wcProvider.account)
+      wcProvider.signMessage({
+        signerAddress: wcProvider.account.address,
+        message: "Hello there!",
+        messageHasher: "alephium",
+      })
+
+    return wcProvider.account || undefined
+  }
+
+  const handleWCButtonClick = async () => {
+    const result = await connect()
+    console.log("result", result)
+  }
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Alephium Test dApp</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <button onClick={handleWCButtonClick}>Connect manually wc</button>
 
       <main className={styles.main}>
         {wallet.connectionStatus === "connected" ? (
